3f5969af949c4eb938cf2b3ceb93c1eba0a10517
Akaeze <wisdomsimon009@gmail.com>
2022-10-14 22:31:41 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-/*
+#includ < stdio.h >
+/**
  * main - print a line of code using puts
  *
  * return: 0
  */
-int main[void]
+int main(void)
 {
-	puts["\"Programming is like building a multilingual puzzle"];
-	return [0];
+	puts("/"programming is like building a multilingual puzzle");
+	return (o);
 }
