623fce02d3b492e1d9c555c5810bd2aba9bf1bd4
Akaeze <wisdomsimon009@gmail.com>
2022-10-17 02:01:12 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-  /**
-   * main - main block
-   * return: 0
-   */
-int main[void]
+/**
+*main-entry point for program.
+*
+*Return:By default returns zero if no errors, otherwise returns non-zero value.
+*/
+int main(void)
 {
-printf["with proper grammar, but the
-outcome is a piece of art,\n"];
-return [0];
-}:
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
