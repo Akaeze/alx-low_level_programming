1ba2575ce8def192e74fb795cd38800eb0b5981f
Akaeze <wisdomsimon009@gmail.com>
2022-10-21 02:41:13 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: simon
+ * description: Header contains protypes used
+ * in 0x03-more_functions_nested_loops directory
+ */
+int _putchar(char c);
+
+#endif
