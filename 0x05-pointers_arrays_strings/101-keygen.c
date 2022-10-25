85b196b27674bfc20fd2f57844f92376abe782e0
Akaeze <wisdomsimon009@gmail.com>
2022-10-25 00:24:22 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -18,7 +18,7 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pss[i] = rand() % 78;
+		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
@@ -32,9 +32,3 @@ int main(void)
 
 	return (0);
 }
-
-
-
-
-
-
